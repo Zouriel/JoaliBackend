2c6da3598bfdfe986cc8709dfb57206ea77e890b
zouriel <mohamed.imdaah@gmail.com>
2025-04-22 21:25:45 +0500

    new migrtion added

---
+++ Migrations/20250422162527_safe.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JoaliBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class safe : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1001,
+                column: "Password_hash",
+                value: "$2a$11$u9kvVVyl2jMsAn.NoVmaFOKNyVgkJMKCmd/j1R4OCKMt61xHHqx2m");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1001,
+                column: "Password_hash",
+                value: "6G94qKPK8LYNjnTllCqm2G3BUM08AzOK7yW30tfjrMc=");
+        }
+    }
+}
